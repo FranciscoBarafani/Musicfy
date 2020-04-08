dd936b39b9ae071dd5dbb33ca5202ed2acea922f
Francisco Barafani <franbarafani@gmail.com>
2020-04-08 17:10:06 -0300

    Register Form now Working

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,41 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Icon, Form, Input } from "semantic-ui-react";
 import "./RegisterForm.scss";
 import firebase from "../../../utils/Firebase";
 import "firebase/auth";
+import { validateEmail } from "../../../utils/Validations";
 
 import "./RegisterForm.scss";
 
 export default function RegisterForm(props) {
   const { setSelectedForm } = props;
+  const [formData, setFormData] = useState(defaultValueForm());
+  const [showPassword, setShowPassword] = useState(false);
+  const [formError, setFormError] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlerShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  //Esta funcion actualiza los datos del form a medida que los editamos
+  const onChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   function onSubmit() {
-    console.log("Skere");
+    setFormError({});
+    let errors = {};
+    let formOk = true;
+
+    if (!validateEmail(formData.email)) {
+      errors.email = true;
+      formOk = false;
+    }
+    if (formData.password.length < 6) {
+      errors.password = true;
+      formOk = false;
+    }
+    if (!formData.username) {
+      errors.username = true;
+      formOk = false;
+    }
+    setFormError(errors);
+
+    if (formOk) {
+      setIsLoading(true);
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(formData.email, formData.password)
+        .then(() => {
+          console.log();
+        })
+        .catch(console.log("Error al crear la cuenta"))
+        .finally(() => {
+          setIsLoading(false);
+          setSelectedForm(null);
+        });
+    }
   }
 
   return (
     <div className="register-form">
       <h1>Empieza a escuchar con una cuenta de Musicfy gratis.</h1>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} onChange={onChange}>
         <Form.Field>
           <Input
             type="text"
             name="email"
             placeholder="Correo Electrónico"
             icon="mail outline"
-            //onChange={}
-            //error={}
+            error={formError.email}
           />
+          {formError.email && (
+            <span className="error-text">
+              Por favor, introduce un correo electrónico valido.
+            </span>
+          )}
         </Form.Field>
         <Form.Field>
           <Input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="Contraseña"
-            icon="eye"
-            //onChange={}
-            //error={}
+            error={formError.password}
+            icon={
+              showPassword ? (
+                <Icon
+                  name="eye slash outline"
+                  link
+                  onClick={handlerShowPassword}
+                />
+              ) : (
+                <Icon name="eye" link onClick={handlerShowPassword} />
+              )
+            }
           />
+          {formError.password && (
+            <span className="error-text">
+              La contraseña debe ser superior a 5 caracteres.
+            </span>
+          )}
         </Form.Field>
         <Form.Field>
           <Input
@@ -43,9 +108,13 @@ export default function RegisterForm(props) {
             name="username"
             placeholder="Como deberiamos llamarte?"
             icon="user circle outline"
-            //onChange={}
-            //error={}
+            error={formError.username}
           />
+          {formError.username && (
+            <span className="error-text">
+              Por favor introduce un nombre de usuario.
+            </span>
+          )}
         </Form.Field>
         <Button type="submit">Continuar</Button>
       </Form>
@@ -58,4 +127,12 @@ export default function RegisterForm(props) {
       </div>
     </div>
   );
+}
+
+function defaultValueForm() {
+  return {
+    email: "",
+    password: "",
+    username: "",
+  };
 }
